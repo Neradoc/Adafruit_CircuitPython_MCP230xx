d50c84789601c11f603e4f2d9638b6f4ce366ae5
Neradoc <neraOnGit@ri1.fr>
2022-05-11 21:18:38 +0200

    unmock micropython

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,10 @@ extensions = [
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-autodoc_mock_imports = ["micropython", "adafruit_bus_device", "digitalio"]
+autodoc_mock_imports = [
+    "adafruit_bus_device",
+    "digitalio",
+]
 
 
 intersphinx_mapping = {
